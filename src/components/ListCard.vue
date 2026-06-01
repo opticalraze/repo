f456f430e06ee4b2ea0db30b77edd66c07777aab
Markus <demarkio@outlook.com>
2026-06-01 13:51:05 -0400

    Changed app icons

--- src/components/ListCard.vue
+++ src/components/ListCard.vue
@@ -15,7 +15,7 @@ defineProps({
         <AppIcon :src="`${pkg.icon}`" class="w-14 h-14" />
         <div class="flex-1 min-w-0">
             <h4 class="font-medium">{{ pkg.name }}</h4>
-            <p class="text-sm text-white/60 line-clamp-1">{{ pkg.description }}</p>
+            <p class="text-sm text-white/60 line-clamp-1">{{ pkg.subtitle }}</p>
             <p class="text-xs text-white/40 mt-1">{{ pkg.version }} • {{ pkg.category }}</p>
         </div>
         <BaseButton>
